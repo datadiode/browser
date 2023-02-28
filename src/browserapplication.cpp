datadiode/browser

commit e901d0555f44a1652c3ba729e3b5b7ed1dd05ae1

Qt's exit() doesn't seem to reliably end in process termination, therefore resort to CRT's exit() to reduce the risk of leaving an occasional zombie process

src/browserapplication.cpp
@@ -331,7 +331,7 @@ void BrowserApplication::quitBrowser()
     }
 
     saveSession();
-    exit(0);
+    ::exit(0); // desperately ensure to not leave a zombie process, as occasionally observed with Qt's exit()
 }
 
 /*!

commit f06b275641e546d696c19c3f14eae23233bd136c

https://github.com/datadiode/browser/commit/e901d05 broke window state persistence

src/browserapplication.cpp
@@ -325,7 +325,7 @@ void BrowserApplication::quitBrowser()
         }
     }
 
-    saveSession();
+    mainWindow()->save(); // implies saveSession()
 
     if (jscoptions) {
         // once again, to see if something changed behind the scenes